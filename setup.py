a6ea09b9ce6b7b671affdac6bbf5b11845229ebc
Jackson Toomey <jacksontoomey@gmail.com>
2016-05-04 20:39:35 -0400

    Added setup.py and bot class

--- setup.py
+++ setup.py
@@ -4,4 +4,9 @@ from setuptools import setup
 setup(
     name="farnsworth",
     version="0.1",
+    url="https://github.com/JacksonToomey/farnsworth",
+    author="Jackson Toomey",
+    author_email="jacksontoomey@gmail.com",
+    license="MIT",
+    packages=["farnsworth"],
 )
